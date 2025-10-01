2f5f96486e80ba1c225f39a3236f453bea5b650c
RaduPetrila-dev <rpetrila@icloud.com>
2025-10-01 22:09:33 +0100

    Update __init__.py

--- src/timeseries_alpha/__init__.py
+++ src/timeseries_alpha/__init__.py
@@ -1,6 +1,7 @@
 from timeseries_alpha.backtest import (
   backtest, 
   prepare_weights_from_signal,
+)
 from timeseries_alpha.metrics import perf_summary
 
 __all__ = ["backtest", "prepare_weights_from_signal", "perf_summary"]
