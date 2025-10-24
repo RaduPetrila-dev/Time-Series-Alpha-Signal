ca3287e42e273b710f2a9c170fa251dfca60671e
RaduPetrila-dev <rpetrila@icloud.com>
2025-10-24 18:58:56 +0100

    Update __init__.py
    Signed-off-by: RaduPetrila-dev <rpetrila@icloud.com>

--- src/time_series_alpha_signal/__init__.py
+++ src/time_series_alpha_signal/__init__.py
@@ -1,6 +1,21 @@
 from .backtest import backtest  # noqa: F401
 from . import signals  # noqa: F401
-from .data import load_synthetic_prices, load_csv_prices, load_yfinance_prices  # noqa: F401
+from .data import (
+    load_synthetic_prices,
+    load_csv_prices,
+    load_yfinance_prices,
+)  # noqa: F401
+from .cv import PurgedKFold, combinatorial_purged_cv  # noqa: F401
+from .labels import daily_volatility, triple_barrier_labels, meta_label  # noqa: F401
+from .features import fracdiff_series, fracdiff_df  # noqa: F401
+from .metrics import (
+    annualised_sharpe,
+    newey_west_tstat,
+    block_bootstrap_ci,
+    deflated_sharpe_ratio,
+)  # noqa: F401
+from .optimizer import enforce_leverage  # noqa: F401
+from .evaluation import cross_validate_sharpe, summarize_fold_metrics  # noqa: F401
 
 __all__ = [
     "backtest",
@@ -8,4 +23,18 @@ __all__ = [
     "load_synthetic_prices",
     "load_csv_prices",
     "load_yfinance_prices",
+    "PurgedKFold",
+    "combinatorial_purged_cv",
+    "daily_volatility",
+    "triple_barrier_labels",
+    "meta_label",
+    "fracdiff_series",
+    "fracdiff_df",
+    "annualised_sharpe",
+    "newey_west_tstat",
+    "block_bootstrap_ci",
+    "deflated_sharpe_ratio",
+    "enforce_leverage",
+    "cross_validate_sharpe",
+    "summarize_fold_metrics",
 ]
